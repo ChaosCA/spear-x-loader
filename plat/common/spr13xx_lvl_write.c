aff54dc828181ec5a9c145e837c4754749d60e5a
mirko <mirko@mirko.linux.vnet>
2011-04-13 15:31:13 +0200

    This patch implement a new write leveling algorithm able to recover slice3 instability issue. What it does is: 1) different DQS-MEM_CLK phase alignment. 2) new delta calculation using the delta average of slices 0,1,2.
    Signed-off-by: Mirko Gardi <mirko.gardi@st.com>

--- plat/common/spr13xx_lvl_write.c
+++ plat/common/spr13xx_lvl_write.c
@@ -144,7 +144,11 @@ void set_wrlvldelay(u32 wrlvl_start, u32 *final_wrlvl_delay)
 				break;
 		}
 
-		final_wrlvl_delay[slice] = wrlvl_delay - 1;
+		/*
+		 * added 1 phase to DQS position
+		 * original was final_wrlvl_delay[slice] = wrlvl_delay - 1
+		 */
+		final_wrlvl_delay[slice] = wrlvl_delay;
 
 		prog_wrlvl_delay(slice, final_wrlvl_delay[slice]);
 		start_wrlvl_delay_mod = wrlvl_start + final_wrlvl_delay[slice];
@@ -249,82 +253,96 @@ void set_dqs_parms(u32 wrlvl_start, u32 *final_wrlvl_delay)
 
 	u32 wr_dq_a_timing_reg, start_wrlvl_delay_mod, slice, dqtim;
 	u32 phy_clk_phase_match_pre, dq_clk_phase_match0_pre, dq_clk_phase_match1_pre;
-	u32 dq_clk_phase_match_pre, dqs_dq_clk_phase_match_delta;
+	u32 dq_clk_phase_match_pre;
 
-	u32 phy_ctrl_reg1_dqsgate_assertval[] = {
+	u32 dqs_dq_clk_phase_match_delta;
+
+	u8 dqs_dq_clk_phase_match_delta_value[DATA_SLICE_MAX];
+	u8 dqs_dq_clk_phase_match_delta_offset[DATA_SLICE_MAX];
+
+	u32 dqs_dq_clk_phase_match_delta_expected;
+
+	u8 start_wrlvl_delay_mod_value[DATA_SLICE_MAX];
+	u8 start_wrlvl_delay_mod_offset_value[DATA_SLICE_MAX];
+
+	static u32 phy_ctrl_reg1_dqsgate_assertval[] = {
 		(0 << 0) | (1 << 3),
 		(0 << 0) | (2 << 3),
 		(1 << 0) | (1 << 3),
 		(1 << 0) | (2 << 3)
 	};
-	u32 phy_ctrl_reg1_dqsgate_deassertval[] = {
+	static u32 phy_ctrl_reg1_dqsgate_deassertval[] = {
 		(0 << 5) | (1 << 8),
 		(0 << 5) | (2 << 8),
 		(1 << 5) | (1 << 8),
 		(1 << 5) | (2 << 8)
 	};
-	u32 phy_ctrl_reg1_dqsoe_assertval[] = {
+	static u32 phy_ctrl_reg1_dqsoe_assertval[] = {
 		(0 << 10) | (1 << 13),
 		(0 << 10) | (2 << 13),
 		(1 << 10) | (1 << 13),
 		(1 << 10) | (2 << 13)
 	};
-	u32 phy_ctrl_reg1_dqsoe_deassertval[] = {
+	static u32 phy_ctrl_reg1_dqsoe_deassertval[] = {
 		(1 << 15) | (2 << 18),
 		(2 << 15) | (1 << 18),
 		(2 << 15) | (2 << 18),
 		(3 << 15) | (1 << 18)
 	};
-	u32 phy_ctrl_reg6_dqs_assertval[] = {
+	static u32 phy_ctrl_reg6_dqs_assertval[] = {
 		(0 << 10) | (1 << 13),
 		(0 << 10) | (2 << 13),
 		(1 << 10) | (1 << 13),
 		(1 << 10) | (2 << 13)
 	};
-	u32 phy_ctrl_reg6_dqs_deassertval[] = {
+	static u32 phy_ctrl_reg6_dqs_deassertval[] = {
 		(1 << 15) | (2 << 18),
 		(2 << 15) | (1 << 18),
 		(2 << 15) | (2 << 18),
 		(3 << 15) | (1 << 18)
 	};
-	u16 phy_ctrl_reg5_assertval[4][15] = {
+	static u16 phy_ctrl_reg5_assertval[4][15] = {
 		{0, 0, 0x08, 0x08, 0x08, 0x08, 0x10, 0x10, 0x10, 0x10, 0x09, 0x09, 0x09, 0x09, 0x11},
 		{0, 0, 0x00, 0x08, 0x08, 0x08, 0x08, 0x10, 0x10, 0x10, 0x10, 0x09, 0x09, 0x09, 0x09},
 		{0, 0, 0x00, 0x00, 0x08, 0x08, 0x08, 0x08, 0x10, 0x10, 0x10, 0x10, 0x09, 0x09, 0x09},
 		{0, 0, 0x00, 0x00, 0x00, 0x08, 0x08, 0x08, 0x08, 0x10, 0x10, 0x10, 0x10, 0x09, 0x09}
 	};
-	u16 phy_ctrl_reg5_deassertval[4][15] = {
+	static u16 phy_ctrl_reg5_deassertval[4][15] = {
 		{0, 0, 0x100, 0x100, 0x100, 0x100, 0x200, 0x200, 0x200, 0x200, 0x120, 0x120, 0x120, 0x120, 0x220},
 		{0, 0, 0x000, 0x100, 0x100, 0x100, 0x100, 0x200, 0x200, 0x200, 0x200, 0x120, 0x120, 0x120, 0x120},
 		{0, 0, 0x000, 0x000, 0x100, 0x100, 0x100, 0x100, 0x200, 0x200, 0x200, 0x200, 0x120, 0x120, 0x120},
 		{0, 0, 0x000, 0x000, 0x000, 0x100, 0x100, 0x100, 0x100, 0x200, 0x200, 0x200, 0x200, 0x120, 0x120}
 	};
-	u32 phy_ctrl_reg1_assertval[] = {
+	static u32 phy_ctrl_reg1_assertval[] = {
 		(0 << 20) | (1 << 23),
 		(0 << 20) | (2 << 23),
 		(1 << 20) | (1 << 23),
 		(1 << 20) | (2 << 23)
 	};
-	u32 phy_ctrl_reg1_deassertval[] = {
+	static u32 phy_ctrl_reg1_deassertval[] = {
 		(1 << 25) | (2 << 28),
 		(2 << 25) | (1 << 28),
 		(2 << 25) | (2 << 28),
 		(3 << 25) | (1 << 28)
 	};
-	u32 phy_ctrl_reg7_assertval[] = {
+	static u32 phy_ctrl_reg7_assertval[] = {
 		(0 << 18) | (1 << 21),
 		(0 << 18) | (2 << 21),
 		(1 << 18) | (1 << 21),
 		(1 << 18) | (2 << 21)
 	};
-	u32 phy_ctrl_reg7_deassertval[] = {
+	static u32 phy_ctrl_reg7_deassertval[] = {
 		(1 << 23) | (2 << 26),
 		(2 << 23) | (1 << 26),
 		(2 << 23) | (2 << 26),
 		(3 << 23) | (1 << 26)
 	};
 
-	/* */
+	/*
+	 * first for cycle
+	 * data collection on dqs_dq_clk_phase_match_delta_value[slice]
+	 * data collection on start_wrlvl_delay_mod_value[slice]
+	 */
 	for (slice = 0; slice < DATA_SLICE_MAX; slice++) {
 
 		start_wrlvl_delay_mod = wrlvl_start + final_wrlvl_delay[slice];
@@ -365,19 +383,87 @@ void set_dqs_parms(u32 wrlvl_start, u32 *final_wrlvl_delay)
 		dqs_dq_clk_phase_match_delta &= 0xF;
 		if (dqs_dq_clk_phase_match_delta > 3)
 			while (1)
-				;
+			;
 
-		dqtim = (start_wrlvl_delay_mod - 2) & 0x7;
+		dqs_dq_clk_phase_match_delta_value[slice] = dqs_dq_clk_phase_match_delta;
+		start_wrlvl_delay_mod_value[slice] = start_wrlvl_delay_mod;
+	}
+
+	/* dqs_dq_clk_phase_match_delta_expected calculation on slices 0,1,2 */
+	dqs_dq_clk_phase_match_delta_expected = (unsigned int) (((dqs_dq_clk_phase_match_delta_value[0] +
+						dqs_dq_clk_phase_match_delta_value[1] +
+						dqs_dq_clk_phase_match_delta_value[2] + (3/2))/3));
+
+	/*
+	 * second for cycle
+	 * start_wrlvl_delay_mod_offset_value calculation
+	 */
+	for (slice = 0; slice < DATA_SLICE_MAX; slice++) {
+			dqs_dq_clk_phase_match_delta_offset[slice] =
+		(u32) (dqs_dq_clk_phase_match_delta_expected -
+			dqs_dq_clk_phase_match_delta_value[slice]);
+
+		start_wrlvl_delay_mod_offset_value[slice] =
+		(u32) (start_wrlvl_delay_mod_value[slice] -
+			dqs_dq_clk_phase_match_delta_offset[slice]);
+	}
+
+	/* third for cycle, write leveling settings */
+	for (slice = 0; slice < DATA_SLICE_MAX; slice++) {
+
+		start_wrlvl_delay_mod = wrlvl_start + final_wrlvl_delay[slice];
+
+		writel_field(phy_ctrl_reg1_dqsgate_assertval[start_wrlvl_delay_mod/4],
+				DQSGATE_ASSERT_MSK, phy_ctrl_reg1 + slice);
+		writel_field(phy_ctrl_reg1_dqsgate_deassertval[start_wrlvl_delay_mod/4],
+				DQSGATE_DEASSERT_MSK, phy_ctrl_reg1 + slice);
+
+		writel_field(phy_ctrl_reg1_dqsoe_assertval[start_wrlvl_delay_mod/4],
+				DQSOE_ASSERT_MSK, phy_ctrl_reg1 + slice);
+		writel_field(phy_ctrl_reg1_dqsoe_deassertval[start_wrlvl_delay_mod/4],
+				DQSOE_DEASSERT_MSK, phy_ctrl_reg1 + slice);
+
+		writel_field(phy_ctrl_reg6_dqs_assertval[start_wrlvl_delay_mod/4],
+				REG6_DQS_ASSERT_MSK, phy_ctrl_reg6 + slice);
+		writel_field(phy_ctrl_reg6_dqs_deassertval[start_wrlvl_delay_mod/4],
+				REG6_DQS_DEASSERT_MSK, phy_ctrl_reg6 + slice);
+
+		phy_clk_phase_match_pre = get_match_pre(slice);
+		dq_clk_phase_match0_pre = get_match0_pre(slice);
+		dq_clk_phase_match1_pre = get_match1_pre(slice);
+
+		if (((dq_clk_phase_match0_pre == 8) && (dq_clk_phase_match1_pre == 9)) ||
+				((dq_clk_phase_match1_pre == 8) && (dq_clk_phase_match0_pre == 9)))
+			dq_clk_phase_match_pre = 0;
+		/* else if ((dq_clk_phase_match0_pre & 0x1) == 0) */
+		else if ((dq_clk_phase_match0_pre & 0x8) == 0)
+			dq_clk_phase_match_pre = dq_clk_phase_match0_pre & 7;
+		else
+			dq_clk_phase_match_pre = dq_clk_phase_match1_pre & 7;
+
+		phy_clk_phase_match_pre &= 0x7; /* only 3 bits[2:0] are significant */
+		if (phy_clk_phase_match_pre > dq_clk_phase_match_pre)
+			dqs_dq_clk_phase_match_delta = (7 - phy_clk_phase_match_pre) + dq_clk_phase_match_pre;
+		else
+			dqs_dq_clk_phase_match_delta = dq_clk_phase_match_pre - phy_clk_phase_match_pre;
+		dqs_dq_clk_phase_match_delta &= 0xF;
+		if (dqs_dq_clk_phase_match_delta > 3)
+			while (1)
+			;
+
+		/* here we use start_wrlvl_delay_mod_offset_value[slice], phy_ctrl_reg4 and phy_ctrl_reg5 are related to DQ phase  */
+		dqtim = (start_wrlvl_delay_mod_offset_value[slice] - 2) & 0x7;
 		wr_dq_a_timing_reg = (dqtim << 0) | (dqtim << 3) | (dqtim << 6) | (dqtim << 9) |
 			(dqtim << 12) | (dqtim << 15) | (dqtim << 18) | (dqtim << 21) | (dqtim << 24);
 
 		writel(wr_dq_a_timing_reg, phy_ctrl_reg4 + slice);
 
-		writel_field(phy_ctrl_reg5_assertval[dqs_dq_clk_phase_match_delta][start_wrlvl_delay_mod],
+		writel_field(phy_ctrl_reg5_assertval[dqs_dq_clk_phase_match_delta][start_wrlvl_delay_mod_offset_value[slice]],
 				REG5_ASSERT_MSK, phy_ctrl_reg5 + slice);
-		writel_field(phy_ctrl_reg5_deassertval[dqs_dq_clk_phase_match_delta][start_wrlvl_delay_mod],
+		writel_field(phy_ctrl_reg5_deassertval[dqs_dq_clk_phase_match_delta][start_wrlvl_delay_mod_offset_value[slice]],
 				REG5_DEASSERT_MSK, phy_ctrl_reg5 + slice);
 
+		/* here we use start_wrlvl_delay_mod, phy_ctrl_reg1 and phy_ctrl_reg7 are related to DQS phase  */
 		writel_field(phy_ctrl_reg1_assertval[start_wrlvl_delay_mod/4],
 				REG1_ASSERT_MSK, phy_ctrl_reg1 + slice);
 		writel_field(phy_ctrl_reg1_deassertval[start_wrlvl_delay_mod/4],
@@ -387,7 +473,6 @@ void set_dqs_parms(u32 wrlvl_start, u32 *final_wrlvl_delay)
 		writel_field(phy_ctrl_reg7_deassertval[start_wrlvl_delay_mod/4],
 				REG7_DEASSERT_MSK, phy_ctrl_reg7 + slice);
 	}
-
 }
 
 void lvl_write(void)
